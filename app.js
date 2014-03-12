ac3353be1eab83388c4198eccf90f7d4881eda3c
David Ewald <ewadav@uw.edu>
2014-03-11 20:12:36 -0700

    addig redmes to track objectives and progress

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var app = express();
 app.configure(function(){
   app.use(express.logger());
   app.use(express.compress());
-  app.set('views', __dirname + '/views');
   app.use(express.cookieParser());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
