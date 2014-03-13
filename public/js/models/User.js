fe4b3b14cf6413e3c9a515c287de75c389636ca7
Michael Beswetherick <mbeswetherick@gmail.com>
2014-03-13 10:10:47 -0700

    gui is def tight

--- public/js/models/User.js
+++ public/js/models/User.js
@@ -25,7 +25,8 @@
       console.log(this.attributes.email);
       $.ajax({
         type: "POST",
-        url: "http://localhost:3033/api/signup",
+        dataType: "jsonp",
+        url: "localhost:3033/api/signup",
         data: {username: this.attributes.userName, 
                 password: this.attributes.password, 
                 email: this.attributes.email
@@ -34,8 +35,8 @@
           self.set(data);
           console.log(data);// data should be in JSON and contain model of this user
         },
-        error: function(){
-          console.log("error posting user")// our custom event
+        error: function(error){
+          console.log(error)// our custom event
         }
       });
 
